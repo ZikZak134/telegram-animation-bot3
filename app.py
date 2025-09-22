2910316980905969c0d74a42c071be71f778bc66
ZikZak134 <gramazeka134@gmail.com>
2025-09-22 23:31:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def webhook():
     abort(400)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7860)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host='0.0.0.0', port=port)
